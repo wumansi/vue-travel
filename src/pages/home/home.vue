de8ccdc0dad9aedef0837ae05ed0cb06f98c4675
wumansi <kaka5628@126.com>
2018-11-20 11:54:26 +0800

    add header

---
+++ src/pages/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+  
+</template>
+
+<script>
+  import HomeHeader from './components/header'
+  export default {
+    name: 'Home',
+    components: {
+      HomeHeader
+    }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/pages/list/list.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>list</div>
+</template>
+
+<script>
+export default {
+  name: 'List'
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div{
+  font-size: 50px;
+}
+</style>
