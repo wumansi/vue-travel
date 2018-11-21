013568b28bd3ca2b6f0b34302764ff9f34eab633
wumansi <kaka5628@126.com>
2018-11-21 14:55:06 +0800

    router-city

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -6,10 +6,12 @@
     	<div class="header-input">
     		<span class="iconfont">&#xe632;</span>
     		输入城市/景点/游玩/主题</div>
-    	<div class="header-right">
-    		{{this.city}}
-    		<span class="iconfont arrow-icon">&#xe64a;</span>
-    	</div>
+    	<router-link to="/city">
+	    	<div class="header-right">
+	    		{{this.city}}
+	    		<span class="iconfont arrow-icon">&#xe64a;</span>
+	    	</div>
+    	</router-link>
 	</div>
 </template>
 
@@ -49,6 +51,7 @@ export default {
 			width: 1.24rem
 			float: right
 			text-align: center
+			color: #fff
 			.arrow-icon
 				font-size: .24rem
 				margin-left: -.04rem,
