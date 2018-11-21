211e2fd37dc7a6fe0e83b8719a77663bc99546b6
wumansi <kaka5628@126.com>
2018-11-21 11:13:41 +0800

    index ajax

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="wrapper">
-	<swiper :options="swiperOption">
+	<swiper :options="swiperOption" v-if="list.length">
     	<!-- slides -->
-	    <swiper-slide v-for="item of swiperList" :key="item.id">
-	    	<img class="swiper-img" :src="item.url">
+	    <swiper-slide v-for="item of list" :key="item.id">
+	    	<img class="swiper-img" :src="item.imgUrl">
 	    </swiper-slide>
 	    <!-- Optional controls -->
 	    <div class="swiper-pagination"  slot="pagination"></div>
@@ -16,25 +16,15 @@
 
   export default {
     name: 'HomeSwiper',
+    props: {
+    	list: Array
+    },
    	data () {
    		return{
    			swiperOption: {
    				pagination: '.swiper-pagination',
    				loop: true
-   			},
-   			swiperList: [{
-				id:'001',
-				url: 'http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg'   			
-			},{
-				id:'002',
-				url: 'http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg'
-			},{
-				id:'003',
-				url: 'http://img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
-			},{
-				id:'004',
-				url: 'http://img1.qunarzz.com/piao/fusion/1712/91/a275569091681d02.jpg_640x200_0519ccb9.jpg'
-			}]
+   			}
    		}
    	}
 }
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <home-header></home-header>
-    <home-swiper></home-swiper>
-    <home-icons></home-icons>
-    <home-recommend></home-recommend>
-    <weekend></weekend>
+    <home-header :city="city"></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <weekend :list="weekendList"></weekend>
   </div>
 </template>
 
@@ -14,20 +14,49 @@
   import HomeIcons from './components/icons'
   import HomeRecommend from './components/recommend'
   import Weekend from './components/weekend'
+  import axios from 'axios'
   export default {
     name: 'Home',
+    data () {
+      return{
+        city: "",
+        swiperList: [],
+        iconList: [],
+        recommendList: [],
+        weekendList: []
+      }
+    },
     components: {
       HomeHeader,
       HomeSwiper,
       HomeIcons,
       HomeRecommend,
       Weekend
+    },
+    methods: {
+      getHomeInfo (){
+        axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+      },
+      getHomeInfoSucc (res) {
+        console.log(res)
+        res = res.data
+        if(res.ret && res.data){
+          var data = res.data
+          this.city = res.city
+          this.swiperList = data.swiperList
+          this.iconList = data.iconList
+          this.recommendList = data.recommendList
+          this.weekendList = data.weekendList
+        }
+      }
+    },
+    mounted () {
+      this.getHomeInfo()
     }
-  
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
